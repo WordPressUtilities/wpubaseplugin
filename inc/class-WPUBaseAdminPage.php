3d05dc5f1cda7ec63fa86c9d602e5ba4081e5b76
Darklg <kev.rocher@gmail.com>
2015-02-26 22:34:12 +0100

    v 1.10
    * Admin pages v 1.2.0 : Separate functions for content & action / Default values / Hide banner menu by default / Code fixes / Uses admin-post for post action

--- inc/class-WPUBaseAdminPage.php
+++ inc/class-WPUBaseAdminPage.php
@@ -3,7 +3,7 @@
 /*
 Class Name: WPU Base Admin page
 Description: A class to handle pages in WordPress
-Version: 1.1.0
+Version: 1.2.0
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -21,12 +21,42 @@ class WPUBaseAdminPage
         'subpage' => array(
             'name' => 'Subpage page',
             'menu_name' => 'Subpage page',
-            'parent' => 'main'
+            'parent' => 'main',
         )
     );
 
+    /* Main
+     -------------------------- */
+
+    function page_content__main() {
+        echo '<p>' . __('Content', 'wpubaseplugin') . ' main</p>';
+        echo '<button class="button-primary" type="submit">' . __('Submit', 'wpubaseplugin') . '</button>';
+    }
+
+    function page_action__main() {
+        $this->parent->messages->set_message('success_postaction_main', 'Success Main !');
+    }
+
+    /* Subpage
+     -------------------------- */
+
+    function page_content__subpage() {
+        echo '<p>' . __('Content', 'wpubaseplugin') . ' subpage</p>';
+        echo '<button class="button-primary" type="submit">' . __('Submit', 'wpubaseplugin') . '</button>';
+    }
+
+    function page_action__subpage() {
+        $this->parent->messages->set_message('success_postaction_subpage', 'Success subpage !');
+    }
+
+    /* ----------------------------------------------------------
+      Script
+    ---------------------------------------------------------- */
+
     function __construct($parent) {
         $this->parent = $parent;
+        $this->prefix = $this->parent->options['id'] . '-';
+        $this->pages = $this->set_pages($this->pages);
         add_action('admin_menu', array(&$this,
             'set_admin_menu'
         ));
@@ -37,23 +67,58 @@ class WPUBaseAdminPage
         // Only on a plugin admin page
         $page = $this->get_page();
         if (array_key_exists($page, $this->pages)) {
-            add_action('wp_loaded', array(&$this,
+            add_action('admin_post_' . $this->parent->options['id'], array(&$this,
                 'set_admin_page_main_postAction'
             ));
         }
     }
 
+    function set_pages($pages) {
+        foreach ($pages as $id => $page) {
+            $page['id'] = $this->prefix . $id;
+            $page['url'] = admin_url('admin.php?page=' . $page['id']);
+            if (!isset($page['name'])) {
+                $page['name'] = $id;
+            }
+            if (!isset($page['menu_name'])) {
+                $page['menu_name'] = $page['name'];
+            }
+            if (!isset($page['parent'])) {
+                $page['parent'] = '';
+            }
+            if (!isset($page['display_banner_menu'])) {
+                $page['display_banner_menu'] = false;
+            }
+            if (!isset($page['function_content'])) {
+                $page['function_content'] = array(&$this,
+                    'page_content__' . $id
+                );
+            }
+            if (!isset($page['function_action'])) {
+                $page['function_action'] = array(&$this,
+                    'page_action__' . $id
+                );
+            }
+            if (!isset($page['level'])) {
+                $page['level'] = $this->parent->options['level'];
+            }
+            $pages[$id] = $page;
+        }
+        return $pages;
+    }
+
     function set_admin_menu() {
         $parent = false;
         foreach ($this->pages as $id => $page) {
-            $page_id_pref = $this->parent->options['id'] . '-';
-            $page_id = $page_id_pref . $id;
+
+            $page_id = $page['id'];
             $page_action = array(&$this,
                 'set_admin_page_main'
             );
-            if (isset($page['parent']) && array_key_exists($page['parent'], $this->pages)) {
-                add_submenu_page($page_id_pref . $page['parent'], $page['name'], $page['menu_name'], $page['level'], $page_id, $page_action);
-            } else {
+            if (array_key_exists($page['parent'], $this->pages)) {
+                add_submenu_page($this->prefix . $page['parent'], $page['name'], $page['menu_name'], $page['level'], $page_id, $page_action);
+            }
+            else {
                 add_menu_page($page['name'], $page['menu_name'], $page['level'], $page_id, $page_action);
             }
         }
@@ -61,18 +126,19 @@ class WPUBaseAdminPage
 
     function set_adminbar_menu($admin_bar) {
         foreach ($this->pages as $id => $page) {
-            $page_id_pref = $this->parent->options['id'] . '-';
-
+            if (!$page['display_banner_menu']) {
+                continue;
+            }
             $menu_details = array(
-                'id' => $page_id_pref . $id,
+                'id' => $page['id'],
                 'title' => $page['menu_name'],
-                'href' => admin_url('admin.php?page=' . $page_id_pref . $id) ,
+                'href' => $page['url'],
                 'meta' => array(
                     'title' => $page['menu_name'],
                 ) ,
             );
             if (isset($page['parent']) && array_key_exists($page['parent'], $this->pages)) {
-                $menu_details['parent'] = $page_id_pref . $page['parent'];
+                $menu_details['parent'] = $this->prefix . $page['parent'];
             }
             $admin_bar->add_menu($menu_details);
         }
@@ -84,25 +150,11 @@ class WPUBaseAdminPage
         echo $this->get_wrapper_start($this->pages[$page]['name']);
 
         // Default Form
-        echo '<form action="" method="post"><div>';
-
+        echo '<form action="' . admin_url('admin-post.php') . '" method="post"><div>';
+        echo '<input type="hidden" name="action" value="' . $this->parent->options['id'] . '">';
+        echo '<input type="hidden" name="page_name" value="' . $page . '" />';
         wp_nonce_field('action-main-form-' . $page, 'action-main-form-' . $this->parent->options['id'] . '-' . $page);
-
-        switch ($page) {
-            case 'main':
-                echo '<p>' . __('Content', 'wpubaseplugin') . '</p>';
-                echo '<button class="button-primary" type="submit">' . __('Submit', 'wpubaseplugin') . '</button>';
-                break;
-
-            case 'subpage':
-                echo '<p>' . __('Content', 'wpubaseplugin') . '</p>';
-                echo '<button class="button-primary" type="submit">' . __('Submit', 'wpubaseplugin') . '</button>';
-                break;
-
-            default:
-                break;
-        }
-
+        call_user_func($this->pages[$page]['function_content']);
         echo '</div></form>';
 
         echo $this->get_wrapper_end();
@@ -114,18 +166,8 @@ class WPUBaseAdminPage
         if (empty($_POST) || !isset($_POST[$action_id]) || !wp_verify_nonce($_POST[$action_id], 'action-main-form-' . $page)) {
             return;
         }
-        switch ($page) {
-            case 'main':
-                $this->parent->messages->set_message('success_postaction', 'Success Main !');
-                break;
-
-            case 'subpage':
-                $this->parent->messages->set_message('success_postaction', 'Success Main 2 !');
-                break;
-
-            default:
-                break;
-        }
+        call_user_func($this->pages[$page]['function_action']);
+        wp_redirect($this->pages[$page]['url']);
     }
 
     private function get_wrapper_start($title) {
@@ -140,6 +182,9 @@ class WPUBaseAdminPage
         $page = '';
         if (isset($_GET['page'])) {
             $page = str_replace($this->parent->options['id'] . '-', '', $_GET['page']);
+        }
+        if (isset($_POST['page_name'])) {
+            $page = str_replace($this->parent->options['id'] . '-', '', $_POST['page_name']);
         }
         return $page;
     }
--- wpubaseplugin.php
+++ wpubaseplugin.php
@@ -4,7 +4,7 @@
 Plugin Name: WPU Base Plugin
 Plugin URI: http://github.com/Darklg/WPUtilities
 Description: A framework for a WordPress plugin
-Version: 1.9
+Version: 1.10
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
