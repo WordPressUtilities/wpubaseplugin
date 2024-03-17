06d1565735bfbb149703f964c55529bbc437e51c
Kevin <kevin@rocher-dev.com>
2024-03-17 22:41:08 +0100

    v 2.72.0
    Protect all subfolders

--- wpubaseplugin.php
+++ wpubaseplugin.php
@@ -5,7 +5,7 @@ Plugin Name: WPU Base Plugin
 Plugin URI: https://github.com/WordPressUtilities/wpubaseplugin
 Update URI: https://github.com/WordPressUtilities/wpubaseplugin
 Description: A framework for a WordPress plugin
-Version: 2.71.1
+Version: 2.72.0
 Author: Darklg
 Author URI: https://darklg.me/
 Text Domain: wpubaseplugin
@@ -20,7 +20,7 @@ defined('ABSPATH') || die;
 
 class WPUBasePlugin {
 
-    public $version = '2.71.1';
+    public $version = '2.72.0';
 
     private $utilities_classes = array(
         'messages' => array(
